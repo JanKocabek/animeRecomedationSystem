46227be81fd20a7e769ad645790bd69a82065b7a
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-07-23 11:30:06 +0200

    Initial commit basic settings MySQL container

---
+++ src/test/java/cz/kocabek/animerecomedationsystem/AnimeRecommendationAppTests.java
@@ -0,0 +1,15 @@
+package cz.kocabek.animerecomedationsystem;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class AnimeRecomedationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
