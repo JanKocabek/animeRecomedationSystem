2f170ddcb5bd0283a626a9bfde6c270a43063153
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-07-31 08:38:27 +0200

    user repository with a complex query for getting user with their anime who rated the one on input

---
+++ src/main/java/cz/kocabek/animerecomedationsystem/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package cz.kocabek.animerecomedationsystem.repository;
+
+import cz.kocabek.animerecomedationsystem.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+//getting users who rated the anime on input and list of anime's they rated
+
+    @EntityGraph(attributePaths = {"usersAnimeScores.anime.genres"})
+    @Query("""
+            select u from User u join fetch u.usersAnimeScores usersAnimeScores
+                        where usersAnimeScores.anime.id = ?1
+            """)
+    <T>
+    List<User> findByUsersAnimeScores_Anime_IdOrderByUsersAnimeScores(@NonNull Long id);
+}
