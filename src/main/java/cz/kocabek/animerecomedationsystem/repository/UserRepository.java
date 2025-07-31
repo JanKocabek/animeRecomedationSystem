9b3c0974a4356d03e6e540fb3d18812f62416c4b
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-07-31 10:33:58 +0200

    adding a custom version of findById for proper matching

--- src/main/java/cz/kocabek/animerecomedationsystem/repository/UserRepository.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package cz.kocabek.animerecomedationsystem.repository;
 
+import cz.kocabek.animerecomedationsystem.dto.UserDto;
 import cz.kocabek.animerecomedationsystem.entity.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,16 +8,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+//just the user without any other things
+    Optional<UserDto> findUserById(Long id);
 
 //getting users who rated the anime on input and list of anime's they rated
-
     @EntityGraph(attributePaths = {"usersAnimeScores.anime.genres"})
     @Query("""
             select u from User u join fetch u.usersAnimeScores usersAnimeScores
                         where usersAnimeScores.anime.id = ?1
             """)
-    <T>
-    List<User> findByUsersAnimeScores_Anime_IdOrderByUsersAnimeScores(@NonNull Long id);
+    List<User> findByUsersAnimeScores_Anime_Id(@NonNull Long id);
 }
