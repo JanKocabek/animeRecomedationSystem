3ad892337d9560bf9162a52eb125e5379a42648d
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-07-31 10:30:29 +0200

    cleaning code

--- src/main/java/cz/kocabek/animerecomedationsystem/AnimeRecommendationApp.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/AnimeRecommendationApp.java
@@ -2,17 +2,9 @@ package cz.kocabek.animerecomedationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class AnimeRecommendationApp {
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello World! Docker";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(AnimeRecommendationApp.class, args);
