efdf4ed1a4d89ea86ea2d339c252da0ad822c78f
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-07-31 08:30:13 +0200

    basic testable methods/queries for getting anime data from DB

--- src/main/java/cz/kocabek/animerecomedationsystem/repository/AnimeRepository.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/repository/AnimeRepository.java
@@ -2,8 +2,13 @@ package cz.kocabek.animerecomedationsystem.repository;
 
 import cz.kocabek.animerecomedationsystem.entity.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 
 
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
+    Iterable<Anime> findTop5ByGenres_GenreNameContainsIgnoreCaseAllIgnoreCase(@NonNull String genreName);
 
+    @Query("select a.id from Anime a where a.name = ?1")
+    Long getIdByName(@NonNull String name);
 }
