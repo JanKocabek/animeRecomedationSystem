3f08eaba1fe07d4a5ddace03cdab671f9224b892
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-07-31 08:32:39 +0200

    simple anime service bean

---
+++ src/main/java/cz/kocabek/animerecomedationsystem/service/AnimeService.java
@@ -0,0 +1,28 @@
+package cz.kocabek.animerecomedationsystem.service;
+
+import cz.kocabek.animerecomedationsystem.entity.Anime;
+import cz.kocabek.animerecomedationsystem.repository.AnimeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnimeService {
+
+    AnimeRepository animeRepository;
+
+    public AnimeService(AnimeRepository animeRepository) {
+        this.animeRepository = animeRepository;
+    }
+
+    public Anime getAnimeById(Long id) {
+        return animeRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Anime not found"));
+    }
+
+    public Iterable<Anime> getAnimeByGenre(String genre){
+        return animeRepository.findTop5ByGenres_GenreNameContainsIgnoreCaseAllIgnoreCase(genre);
+    }
+
+    public Long getAnimeIdByName(String name){
+        return animeRepository.getIdByName(name);
+    }
+
+}
