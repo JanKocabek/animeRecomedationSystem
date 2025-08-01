724bd9a43cf8f5d5925896fca7082601146e11ac
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-08-01 15:15:07 +0200

    change the query to get sorted results based on user id change results into slice to get just a portion of results

--- src/main/java/cz/kocabek/animerecomedationsystem/repository/UsersAnimeScoreRepository.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/repository/UsersAnimeScoreRepository.java
@@ -4,13 +4,18 @@ import cz.kocabek.animerecomedationsystem.dto.UsersAnimeScoreDto;
 import cz.kocabek.animerecomedationsystem.entity.UsersAnimeScore;
 import cz.kocabek.animerecomedationsystem.entity.UsersAnimeScoreId;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface UsersAnimeScoreRepository extends JpaRepository<UsersAnimeScore, UsersAnimeScoreId> {
-    List<UsersAnimeScoreDto> getUsersAnimeScoresById_UserIdInAndId_AnimeIdNotNull(@NonNull Collection<Long> userIds, Pageable pageable);
+    @Query("""
+            select new cz.kocabek.animerecomedationsystem.dto.UsersAnimeScoreDto(u.id.userId,u.id.animeId,u.animeTitle,u.rating)  from UsersAnimeScore u
+            where u.id.userId in :userIds and u.id.animeId is not null
+            order by u.id.userId""")
+    Slice<UsersAnimeScoreDto> getUsersListRatedAnime(@Param("userIds") @NonNull Collection<Long> userIds, @Param("animeId")  Pageable pageable);
 }
-
