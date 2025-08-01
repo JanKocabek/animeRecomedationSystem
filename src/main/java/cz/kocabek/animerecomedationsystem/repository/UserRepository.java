3370d4d85e78e2eb0bd3d8bc6b43b12dcdabd42d
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-08-01 15:22:18 +0200

    slicing only a top part of users for faster working

--- src/main/java/cz/kocabek/animerecomedationsystem/repository/UserRepository.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/repository/UserRepository.java
@@ -1,17 +1,17 @@
 package cz.kocabek.animerecomedationsystem.repository;
 
 import cz.kocabek.animerecomedationsystem.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
-
-import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("""
                         select u.id from User u join u.usersAnimeScores uas
                         where uas.id.animeId = ?1 order by uas.rating desc
             """)
-    List<Long> retrieveDistinctUserIdsByAnimeSorted(@NonNull Long animeId);
+    Slice<Long> retrieveDistinctUserIdsByAnimeSorted(@NonNull Long animeId, Pageable pageable);
 
 }
--- src/main/java/cz/kocabek/animerecomedationsystem/service/UserServices.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/service/UserServices.java
@@ -1,6 +1,7 @@
 package cz.kocabek.animerecomedationsystem.service;
 
 import cz.kocabek.animerecomedationsystem.repository.UserRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,36 +15,8 @@ public class UserServices {
         this.userRepository = userRepository;
     }
 
-//    public Iterable<UserDto> getAllUsersWhoRatedGiven(Long id) {
-//        List<User> users = new ArrayList<>();
-//        try {
-//            users = userRepository.findByUsersAnimeScores_Anime_Id(id);
-//        } catch (Exception e) {
-//            e.printStackTrace(System.err);
-//        }
-//
-//
-//        if (users.isEmpty()) {
-//            return null;
-//        }
-//        final var batch = users.subList(0, Math.min(users.size(), 5));
-//        List<UserDto> uDTO = new ArrayList<>();
-//        for (User user : batch) {
-//            Set<UsersAnimeScoreDto> uasDTO = new LinkedHashSet<>();
-//            for (UsersAnimeScore uas : user.getUsersAnimeScores()) {
-//                Anime a = uas.getAnime();
-//                AnimeDto aDTO = new AnimeDto(a.getName());
-//                UsersAnimeScoreDto UsersAnimeScoreDto = new UsersAnimeScoreDto(aDTO, uas.getRating());
-//                uasDTO.add(UsersAnimeScoreDto);
-//            }
-//            //uDTO.add(new UserDto(user.getId(), uasDTO));
-//        }
-//        return uDTO;
-//    }
-
-
     public List<Long> getUserWithAnime(Long aniId) {
-        return userRepository.retrieveDistinctUserIdsByAnimeSorted(aniId);
+        return userRepository.retrieveDistinctUserIdsByAnimeSorted(aniId, PageRequest.of(0, 5)).getContent();
     }
 
 }
