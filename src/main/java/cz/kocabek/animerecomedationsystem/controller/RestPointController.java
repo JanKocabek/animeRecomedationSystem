49471098d4c6b28b5427accc58ce527305421271
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-08-04 12:03:29 +0200

    changing two of the current controls into the rest one to build thymeleaf one separately

--- src/main/java/cz/kocabek/animerecomedationsystem/controller/RestPointController.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/controller/RestPointController.java
@@ -5,41 +5,40 @@ import cz.kocabek.animerecomedationsystem.repository.UsersAnimeScoreRepository;
 import cz.kocabek.animerecomedationsystem.service.AnimeService;
 import cz.kocabek.animerecomedationsystem.service.RecommendationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
-public class ViewController {
+@RestController
+public class RestPointController {
 
     AnimeService animeService;
     UsersAnimeScoreRepository usersAnimeScoreRepository;
     RecommendationService recommendationService;
 
-    public ViewController(AnimeService animeService, UsersAnimeScoreRepository usersAnimeScoreRepository, RecommendationService recommendationService) {
+    public RestPointController(AnimeService animeService, UsersAnimeScoreRepository usersAnimeScoreRepository, RecommendationService recommendationService) {
         this.animeService = animeService;
         this.usersAnimeScoreRepository = usersAnimeScoreRepository;
         this.recommendationService = recommendationService;
     }
 
-    @ResponseBody
+
     @GetMapping("/anime/{id}")
     public ResponseEntity<Anime> getAnimeById(@PathVariable Long id) {
         Anime anime = animeService.getAnimeById(id);
         return ResponseEntity.ok(anime);
     }
 
-    @ResponseBody
+
     @GetMapping("/anime/genre/{genre}")
     public ResponseEntity<Iterable<Anime>> getAnimeByGenre(@PathVariable String genre) {
         Iterable<Anime> anime = animeService.getAnimeByGenre(genre);
         return ResponseEntity.ok(anime);
     }
 
-    @ResponseBody
+
     @GetMapping("/anime/recommend/{name}")
     public ResponseEntity<Map<String, Integer>> getAnimeRecommendation(@PathVariable String name) {
         Map<String, Integer> data = recommendationService.getAnimeRecommendation(name);
