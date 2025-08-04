bbcbc10ed1919b886c6a2ed4f1ea9e3212c07bad
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-08-04 17:19:26 +0200

    creating separate submit post method and result view endpoint

--- src/main/java/cz/kocabek/animerecomedationsystem/controller/ViewController.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/controller/ViewController.java
@@ -1,6 +1,8 @@
 package cz.kocabek.animerecomedationsystem.controller;
 
 import cz.kocabek.animerecomedationsystem.dto.FormData;
+import cz.kocabek.animerecomedationsystem.service.AnimeService;
+import cz.kocabek.animerecomedationsystem.service.RecommendationService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,20 +10,45 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ViewController {
+
+    RecommendationService recommendationService;
+    AnimeService animeService;
+
+    public ViewController(RecommendationService recommendationService, AnimeService animeService) {
+        this.recommendationService = recommendationService;
+        this.animeService = animeService;
+    }
 
     @GetMapping("/")
     public String getHomePage(Model model) {
         model.addAttribute("anime", new FormData());
         return "index";
     }
+
     @PostMapping("/submit")
-    public String postHomePage(@Valid @ModelAttribute("anime") FormData data, BindingResult bindingResult) {
+    public String postHomePage(@Valid @ModelAttribute("anime") FormData data, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
             return "index";
         }
-        return "index";
+        data.setAnimeName(data.getAnimeName().toLowerCase());
+        final var id = animeService.getAnimeIdByName(data.getAnimeName());
+        if (id == null) {
+            return "index";
+        }
+        redirectAttributes.addAttribute("id", id);
+        return "redirect:/result";
+    }
+
+    @GetMapping("/result")
+    public String getResultPage(@RequestParam Long id, Model model) {
+        final var recommendationMap = recommendationService.getAnimeRecommendation(id);
+        model.addAttribute("recommendationMap", recommendationMap);
+        model.addAttribute("anime", new FormData());
+        return "result";
     }
 }
