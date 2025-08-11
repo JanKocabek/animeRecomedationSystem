686711a90e55c7452ad33060a7c75529c4b251ce
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-08-11 21:37:10 +0200

    new feature: fetching Anime Genres

--- src/main/java/cz/kocabek/animerecomedationsystem/service/RecommendationService.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/service/RecommendationService.java
@@ -4,6 +4,7 @@ import cz.kocabek.animerecomedationsystem.dto.AnimeDto;
 import cz.kocabek.animerecomedationsystem.dto.AnimeOutDTO;
 import cz.kocabek.animerecomedationsystem.dto.RecommendationDTO;
 import cz.kocabek.animerecomedationsystem.dto.UserAnimeList;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +23,7 @@ import java.util.Map;
  * - {@link UserAnimeScoreService} for fetching user-anime relationship data and ratings.
  * - {@link RecommendationEngine} to execute the recommendation algorithm and derive anime suggestions.
  */
+@AllArgsConstructor
 @Service
 public class RecommendationService {
     private static final Logger logger = LoggerFactory.getLogger(RecommendationService.class);
@@ -30,13 +32,8 @@ public class RecommendationService {
     UserAnimeScoreService userAnimeScoreService;
     RecommendationEngine engine;
     DTOResultBuilder resultBuilder;
+    AnimeGenreService animeGenreService;
 
-    public RecommendationService(AnimeService animeService, RecommendationEngine engine, UserAnimeScoreService userAnimeScoreService, DTOResultBuilder resultBuilder) {
-        this.animeService = animeService;
-        this.engine = engine;
-        this.userAnimeScoreService = userAnimeScoreService;
-        this.resultBuilder = resultBuilder;
-    }
 
     /* public endpoints*/
     public RecommendationDTO getAnimeRecommendation(String name) {
@@ -64,11 +61,13 @@ public class RecommendationService {
         long step1Duration = (System.nanoTime() - step1Start) / 1_000_000;
         logger.warn("Step 1 (collect users data) took: {} ms", step1Duration);
         logger.info("size of data after grouping: {}", userAnimeData.size());
+
         long step2Start = System.nanoTime();
         final var sortedMap = engine.buildAnimeOccurrencesMap(userAnimeData);
         long step2Duration = (System.nanoTime() - step2Start) / 1_000_000;
         logger.warn("Step 2 (build anime occurrences map) took: {} ms", step2Duration);
         logger.debug("number of anime in map: {}", sortedMap.size());
+
         long step3Start = System.nanoTime();
         final var weightedAnime = engine.weightAnime(sortedMap, AnimeScore.compositeScoring);
         final var topRecommendations = engine.cutTheTopN(weightedAnime);//current final map with ids without detail yet
@@ -76,7 +75,8 @@ public class RecommendationService {
         logger.warn("Step 3 (weight anime) took: {} ms", step3Duration);
         logger.debug("size of shortened list: {}", topRecommendations.size());
         long step4Start = System.nanoTime();
-        final var animeDetailsList = animeService.getListAnimeFromIds(getAnimeIDsFromDTO(topRecommendations));
+        final var animeDetailsList = getAnimeDetails(topRecommendations);
+
         long step4Duration = (System.nanoTime() - step4Start) / 1_000_000;
         logger.warn("Step 4 (get anime details) took: {} ms", step4Duration);
         logger.debug("recommended anime: {}", animeDetailsList.size());
@@ -89,10 +89,12 @@ public class RecommendationService {
         long step1_1Duration = (System.nanoTime() - step1_1Start) / 1_000_000;
         logger.warn("Step 1.1 (collect users with anime) took: {} ms", step1_1Duration);
         logger.info("Users with anime after service: {}", usersId.size());
+
         long step1_2Start = System.nanoTime();
         final var userRatingsData = userAnimeScoreService.fetchRatedAnimeByUsers(usersId, animeId, Pageable.unpaged());
         long step1_2Duration = (System.nanoTime() - step1_2Start) / 1_000_000;
         logger.warn("Step 1.2 (fetch user ratings) took: {} ms", step1_2Duration);
+
         return engine.groupUserByID(userRatingsData);
     }
 
@@ -102,7 +104,7 @@ public class RecommendationService {
 
     private RecommendationDTO buildOutputList(List<AnimeDto> animeDetails, Map<Long, AnimeOutDTO> outDTO) {
         for (AnimeDto detail : animeDetails) {
-            outDTO.get(detail.id()).setAnimeInfo(detail);
+            outDTO.get(detail.getId()).setAnimeInfo(detail);
         }
         return resultBuilder.addRecommendation(convertToAnimeList(outDTO)).build();
     }
@@ -110,6 +112,15 @@ public class RecommendationService {
     private List<AnimeOutDTO> convertToAnimeList(Map<Long, AnimeOutDTO> animeData) {
         animeData.forEach((key, value) -> value.setAnimeId(key));
         return animeData.values().stream().toList();
+    }
+
+    private List<AnimeDto> getAnimeDetails(Map<Long, AnimeOutDTO> topRecommendations) {
+        final var details = animeService.getListAnimeFromIds(getAnimeIDsFromDTO(topRecommendations));
+        final var genresDetail = animeGenreService.getGenreForAnime(topRecommendations.keySet());
+        for (AnimeDto detail : details) {
+            detail.getGenres().addAll(genresDetail.get(detail.getId()));
+        }
+        return details;
     }
 }
 
