40ac3ad56490e67f4635ce7dfb3e5c948291f140
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-08-14 18:07:14 +0200

    move the rest endpoint into own sub endpoints 'api/' to not collide with views one points

--- src/main/java/cz/kocabek/animerecomedationsystem/controller/RestPointController.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/controller/RestPointController.java
@@ -8,8 +8,10 @@ import cz.kocabek.animerecomedationsystem.service.RecommendationService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api")
 @RestController
 public class RestPointController {
 
