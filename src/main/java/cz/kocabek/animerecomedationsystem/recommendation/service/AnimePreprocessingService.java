9437f1b022e377e1129ea526d53b42db6ea916f7
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-08-21 14:57:05 +0200

    decoupling retrieving genres from filter Logic to make it filter single responsible

--- src/main/java/cz/kocabek/animerecomedationsystem/recommendation/service/AnimePreprocessingService.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/recommendation/service/AnimePreprocessingService.java
@@ -6,6 +6,7 @@ import cz.kocabek.animerecomedationsystem.recommendation.service.db.AnimeGenreSe
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -21,12 +22,14 @@ public class AnimePreprocessingService {
 
     public Map<Long, AnimeOutDTO> filterAnime(Map<Long, AnimeOutDTO> animeMap) {
         final var map = new HashMap<>(animeMap);
-        if (config.isOnlyInAnimeGenres()) filterOutByInputGenres(map, config.getAnimeId());
+        if (config.isOnlyInAnimeGenres()) {
+            final var genres = animeGenreService.getGenresForAnime(config.getAnimeId());
+            filterOutByInputGenres(map, genres);
+        }
         return map;
     }
 
-    private void filterOutByInputGenres(Map<Long, AnimeOutDTO> map, Long animeId) {
-        final var genres = animeGenreService.getGenresForAnime(animeId);
+    private void filterOutByInputGenres(Map<Long, AnimeOutDTO> map, List<String> genres) {
         map.entrySet().removeIf(entry ->
                 entry.getValue().getGenres().stream().noneMatch(genres::contains));
     }
