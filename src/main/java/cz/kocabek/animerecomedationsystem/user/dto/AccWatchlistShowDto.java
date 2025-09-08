cc9ad879ea2889211851dee6507bf436fdf7288d
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-09-08 17:26:26 +0200

    Added `AccWatchlistShowDto` for simplified watchlist data representation, extended `AccWatchlistRepository` with `getActiveWatchlistByAccountId` query, and updated `AccService` with watchlist data retrieval method.

---
+++ src/main/java/cz/kocabek/animerecomedationsystem/user/dto/AccWatchlistShowDto.java
@@ -0,0 +1,16 @@
+package cz.kocabek.animerecomedationsystem.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link cz.kocabek.animerecomedationsystem.user.entity.AccWatchlist}
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record AccWatchlistShowDto(
+        Long idAnimeId,
+        String animeName,
+        String animeEnglishName,
+        Double animeScore) implements Serializable {
+}
