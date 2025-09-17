74577c5efb78803e0e5183d6a98dd11b37602292
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-09-17 21:48:27 +0200

    Fix: Update SQL script for creating anime table and genres

--- utils/sqlFiles/creatingTables.sql
+++ utils/sqlFiles/creatingTables.sql
@@ -1,31 +1,87 @@
+-- Create Anime table--
+CREATE TABLE
+  anime (
+    anime_id INT UNSIGNED NOT NULL PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    `English name` VARCHAR(255),
+    Score DOUBLE,
+    Type TEXT,
+    Episodes BIGINT,
+    Status TEXT,
+    Source TEXT,
+    Rating TEXT,
+    Rank INT,
+    Popularity INT,
+    `Image URL` TEXT
+  );
+
 -- Create genres table
-CREATE TABLE genres
-(
-    genre_id   INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE
+  genres (
+    genre_id INT AUTO_INCREMENT PRIMARY KEY,
     genre_name VARCHAR(50) UNIQUE
-);
+  );
 
 -- Extract unique genres from CSV data
-INSERT INTO genres (genre_name)
-SELECT DISTINCT TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(Genres_backup, ',', n.digit + 1), ',', -1)) AS genre_name
-FROM anime
-         JOIN (SELECT 0 digit UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4) n
-WHERE LENGTH(Genres_backup) - LENGTH(REPLACE(Genres_backup, ',', '')) >= n.digit
-  AND TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(Genres_backup, ',', n.digit + 1), ',', -1)) != '';
+INSERT INTO
+  genres (genre_name)
+SELECT DISTINCT
+  TRIM(
+    SUBSTRING_INDEX (
+      SUBSTRING_INDEX (Genres, ',', n.digit + 1),
+      ',',
+      -1
+    )
+  ) AS genre_name
+FROM
+  anime
+  JOIN (
+    SELECT
+      0 digit
+    UNION ALL
+    SELECT
+      1
+    UNION ALL
+    SELECT
+      2
+    UNION ALL
+    SELECT
+      3
+    UNION ALL
+    SELECT
+      4
+  ) n
+WHERE
+  LENGTH (Genres) - LENGTH (REPLACE (Genres, ',', '')) >= n.digit
+  AND TRIM(
+    SUBSTRING_INDEX (
+      SUBSTRING_INDEX (Genres, ',', n.digit + 1),
+      ',',
+      -1
+    )
+  ) != '';
 
 -- Create junction table and populate
-CREATE TABLE anime_genres
-(
+CREATE TABLE
+  anime_genres (
     anime_id INT,
     genre_id INT,
     FOREIGN KEY (anime_id) REFERENCES anime (anime_id),
     FOREIGN KEY (genre_id) REFERENCES genres (genre_id)
-);
+  );
 
-INSERT INTO anime_genres(anime_id, genre_id)
-SELECT DISTINCT a.anime_id,
-                g.genre_id
-FROM anime a
-         JOIN genres g ON FIND_IN_SET(g.genre_name, REPLACE(a.Genres_backup, ', ', ',')) > 0 #erasing space between genres so the genres act as SET
-WHERE a.Genres_backup IS NOT NULL
-  AND a.Genres_backup != '';
+INSERT INTO
+  anime_genres (anime_id, genre_id)
+SELECT DISTINCT
+  a.anime_id,
+  g.genre_id
+FROM
+  anime a
+  JOIN genres g ON FIND_IN_SET (
+    g.genre_name,
+    REPLACE (a.Genres, ', ', ',')
+  ) > 0
+  
+WHERE
+  a.Genres IS NOT NULL
+  AND a.Genres != '';
