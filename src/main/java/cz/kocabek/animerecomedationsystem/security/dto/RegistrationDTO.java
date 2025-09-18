bf29c2e308371d8cb9c9ee4493c56d7baba1c892
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-09-18 13:30:42 +0200

    Refactor: simplified user name regex check and Improve formatting and readability of RegistrationDTO fields

--- src/main/java/cz/kocabek/animerecomedationsystem/security/dto/RegistrationDTO.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/security/dto/RegistrationDTO.java
@@ -6,9 +6,13 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RegistrationDTO(
-        @Size(min = 5, message = "User Name must be at least 5 character long") @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "User Name can contain only letters, numbers and underscore.") String username,
-        @Password String password,
+        @Size(min = 5, message = "User Name must be at least 5 character long")
+        @Pattern(regexp = "^[\\w]+$", message = "User Name can contain only letters, numbers and underscore.")
+        String username,
+        @Password
+        String password,
         RoleType role) {
+
     public RegistrationDTO() {
         this("", "", RoleType.USER);
     }
