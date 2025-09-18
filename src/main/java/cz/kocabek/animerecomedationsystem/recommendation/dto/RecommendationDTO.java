b7bdd8de0239130060837c23d6f3ee8d2a0a9b99
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-09-18 16:30:18 +0200

    Fix: Add serialVersionUID to RecommendationDTO

--- src/main/java/cz/kocabek/animerecomedationsystem/recommendation/dto/RecommendationDTO.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/recommendation/dto/RecommendationDTO.java
@@ -2,11 +2,14 @@ package cz.kocabek.animerecomedationsystem.recommendation.dto;
 
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 
 @Data
 public class RecommendationDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 2L;
     private List<String> inputAnimeNames;
     private List<AnimeOutDTO> recommendedAnime;
 }
