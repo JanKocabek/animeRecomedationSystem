206092240163489ef0d2bf23c279865c4092cd93
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-10-06 11:35:37 +0200

    Moving BcryptPasswordEncoder bean into own configuration class, simplified Password service with Lombok annotation, adding throw catch if password wasn't properly updated

--- src/main/java/cz/kocabek/animerecomedationsystem/security/service/PasswordService.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/security/service/PasswordService.java
@@ -1,34 +1,34 @@
 package cz.kocabek.animerecomedationsystem.security.service;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.NoTransactionException;
 import org.springframework.transaction.annotation.Transactional;
 
 import cz.kocabek.animerecomedationsystem.account.UserSessionData;
 import cz.kocabek.animerecomedationsystem.account.repository.AppAccRepository;
 import cz.kocabek.animerecomedationsystem.security.dto.SettingDTO;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class PasswordService {
 
     private final AppAccRepository appAccRepository;
     private final UserSessionData userSessionData;
-    private final BCryptPasswordEncoder encoder;
-
-    PasswordService(AppAccRepository appAccRepository, UserSessionData userSessionData) {
-        this.appAccRepository = appAccRepository;
-        this.userSessionData = userSessionData;
-        encoder = new BCryptPasswordEncoder();
-    }
+    private final PasswordEncoder passwordEncoder;
 
     public boolean checkPassword(SettingDTO setting) {
         final var oldPass = appAccRepository.findAccountPasswordById(userSessionData.getUserId());
-        return encoder.matches(setting.oldPass(), oldPass);
+        return passwordEncoder.matches(setting.oldPass(), oldPass);
     }
 
     @Transactional
     public void changePassword(SettingDTO setting) {
-        final var newHash = encoder.encode(setting.newPass());
-        appAccRepository.updatePassword(userSessionData.getUserId(), newHash);
+        final var newHash = passwordEncoder.encode(setting.newPass());
+        final var rows = appAccRepository.updatePassword(userSessionData.getUserId(), newHash);
+        if (rows != 1) {
+            throw new NoTransactionException("");
+        }
     }
 }
