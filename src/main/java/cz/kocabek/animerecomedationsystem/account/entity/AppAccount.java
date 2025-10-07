c7ec755934b2c43b157adfb38d8d832880365491
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-10-07 10:48:04 +0200

    Fix createdAt initialization in AppAccount to prevent overwriting existing timestamp

--- src/main/java/cz/kocabek/animerecomedationsystem/account/entity/AppAccount.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/account/entity/AppAccount.java
@@ -72,6 +72,8 @@ public class AppAccount {
 
     @PrePersist
     public void setCreatedAt() {
-        this.createdAt = Instant.now();
+        if (this.createdAt == null) {
+            this.createdAt = Instant.now();
+        }
     }
 }
