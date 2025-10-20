daa9c7573ca2b099a6160ca4c5bddfd92a61e8ea
Jan Kocábek <166728580+JanKocabek@users.noreply.github.com>
2025-10-20 22:54:00 +0200

    ✨ feat(recommendation): implement cacheable anime data provider
    - add caching mechanism for fetching rated anime by users
    - improve performance by caching frequently accessed anime rating data
    - use spring cache abstraction for caching user anime scores

--- src/main/java/cz/kocabek/animerecomedationsystem/recommendation/service/db/CacheableAnimeDataProvider.java
+++ src/main/java/cz/kocabek/animerecomedationsystem/recommendation/service/db/CacheableAnimeDataProvider.java
@@ -2,12 +2,17 @@ package cz.kocabek.animerecomedationsystem.recommendation.service.db;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import cz.kocabek.animerecomedationsystem.recommendation.dto.ConfigCacheKey;
+import cz.kocabek.animerecomedationsystem.recommendation.dto.UsersAnimeScoreDto;
 import cz.kocabek.animerecomedationsystem.recommendation.repository.UsersAnimeScoreRepository;
 import cz.kocabek.animerecomedationsystem.recommendation.service.RecommendationConfig.ConfigConstant;
 import lombok.AllArgsConstructor;
@@ -17,6 +22,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CacheableAnimeDataProvider {
 
+    private static final Logger logger = LoggerFactory.getLogger(CacheableAnimeDataProvider.class);
+
     private final UsersAnimeScoreRepository usersAnimeScoreRepository;
 
     @Cacheable(value = "usersWithAnimeScore")
@@ -24,6 +31,14 @@ public class CacheableAnimeDataProvider {
         return usersAnimeScoreRepository.findUsersIdByAnimeIdAndRatingRange(config.animeId(), config.minScore(),
                 ConfigConstant.MAX_INPUT_SCORE,
                 PageRequest.of(0, config.maxUsers())).getContent();
+    }
+
+    @Cacheable(value = "usersListRatedAnime")
+    public Slice<UsersAnimeScoreDto> fetchRatedAnimeByUsers(List<Long> usersId, ConfigCacheKey config) {
+        final var ratedAnimeData = usersAnimeScoreRepository.getUsersListRatedAnime(usersId, config.animeId(), Pageable.unpaged());
+        logger.debug("size of fetch data:  {}", ratedAnimeData.getContent().size());
+        //logger.debug("size of fetch Animedata: {} vs asked: {}", ratedAnimeData.getNumberOfElements(), limit);
+        return ratedAnimeData;
     }
 
 }
